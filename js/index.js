e782ded604cf245b6872ac0bd18927ee93740c5e
Alexander Heraimenka <a.heraimenka@gmail.com>
2020-05-05 13:09:15 -0700

    Add mouseover event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
-// Your code goes here
+/*
+
+* [x] `mouseover`
+* [ ] `keydown`
+* [ ] `wheel`
+* [ ] `drag / drop`
+* [ ] `load`
+* [ ] `focus`
+* [ ] `resize`
+* [ ] `scroll`
+* [ ] `select`
+* [ ] `dblclick`
+
+*/
+
+
+// mouseover + mouseout
+
+let images = document.querySelectorAll('img');
+
+for (let img of images) {
+    img.addEventListener('mouseover', (event) => {
+        event.target.style.transform = "scale(1.1)";
+        event.target.style.transition = "1s ease";
+    });
+    
+    img.addEventListener('mouseout', (event) => {
+        event.target.style.transform = "";
+    });
+}
+
